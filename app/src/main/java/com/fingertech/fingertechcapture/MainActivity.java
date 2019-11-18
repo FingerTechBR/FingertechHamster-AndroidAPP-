51b3c7b4f467ae727aba37f9550a039d9194cfcc
LincolnBerlick <lincoln.berlick@gmail.com>
2019-11-18 16:17:32 -0300

    git inicial

---
+++ app/src/main/java/com/fingertech/fingertechcapture/MainActivity.java
@@ -0,0 +1,206 @@
+package com.fingertech.fingertechcapture;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+
+import com.nitgen.SDK.AndroidBSP.NBioBSPJNI;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class MainActivity extends AppCompatActivity implements Nitgen.View {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private Nitgen nitgen;
+
+
+    @BindView(R.id.btn_captura)  Button btn_captura;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        //personalizado
+
+
+        onCheckPermission();
+        //btn_captura.setEnabled(false);
+        nitgen = new Nitgen(this, this);
+        nitgen.openDevice();
+
+
+
+    }
+
+
+
+
+    public void onCheckPermission(){
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissionArray = new ArrayList<>();
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                permissionArray.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                permissionArray.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                permissionArray.add(Manifest.permission.CAMERA);
+            }
+            if (permissionArray.size() > 0) {
+                String[] strArr = new String[permissionArray.size()];
+                strArr = permissionArray.toArray(strArr);
+                ActivityCompat.requestPermissions(this, strArr, 0);
+            }
+        }
+    }
+
+
+
+
+
+    @OnClick(R.id.btn_captura)
+    public void Captura(){
+
+
+
+        nitgen.onCapture1(10000);
+
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onDeviceConnected() {
+
+    }
+
+    @Override
+    public void onDeviceDisconnected() {
+
+    }
+
+    @Override
+    public void onCapture(NBioBSPJNI.CAPTURED_DATA capturedData) {
+
+    }
+
+    @Override
+    public void onDeviceMessage(String msg) {
+
+    }
+
+    @Override
+    public void onInforMessage(String msg) {
+        runOnUiThread(() -> {
+            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+        });
+    }
+    @Override
+    public void onVersion(String msg) {
+
+    }
+
+    @Override
+    public void showToast(String msg) {
+        runOnUiThread(() -> {
+            Toast.makeText(this,"primeira",Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void setISOButton(boolean enable) {
+
+    }
+
+    @Override
+    public void setRAWButton(boolean enable) {
+
+    }
+}
