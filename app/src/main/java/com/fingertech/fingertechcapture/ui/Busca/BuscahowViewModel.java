a3e289776ca3604d72e65a8cd12ecaca4840d2f5
LincolnBerlick <lincoln.berlick@gmail.com>
2019-12-06 17:50:04 -0300

    tela de busca, criado indexsearch busca de 1 para N digital

--- app/src/main/java/com/fingertech/fingertechcapture/ui/Busca/BuscahowViewModel.java
+++ app/src/main/java/com/fingertech/fingertechcapture/ui/Busca/BuscahowViewModel.java
@@ -1,14 +1,14 @@
-package com.fingertech.fingertechcapture.ui.slideshow;
+package com.fingertech.fingertechcapture.ui.Busca;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class BuscahowViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public BuscahowViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
