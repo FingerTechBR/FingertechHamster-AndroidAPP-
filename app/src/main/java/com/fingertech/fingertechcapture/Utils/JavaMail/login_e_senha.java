789aeef54e8406b2b77ab4deb31ddf88f68b2d0c
LincolnBerlick <lincoln.berlick@gmail.com>
2019-12-20 16:43:47 -0300

    adicionado ignore

--- app/src/main/java/com/fingertech/fingertechcapture/Utils/JavaMail/login_e_senha.java
+++
@@ -1,8 +0,0 @@
-package com.fingertech.fingertechcapture.Utils.JavaMail;
-
-public class login_e_senha {
-
-
-    public static String login = "teste@gmail.com";
-    public static String senha = "teste23f";
-}
